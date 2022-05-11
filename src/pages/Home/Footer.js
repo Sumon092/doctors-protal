c06a2f639751cad919c34f4bb25b6f0d39d271d9
sumon092 <m.sumon@outlook.com>
2022-05-11 18:07:07 +0600

    footer added

---
+++ src/pages/Home/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import footer from '../../assets/images/footer.png'
+
+const Footer = () => {
+    return (
+        <div className='px-12'>
+            <footer style={{
+                background: `url(${footer})`,
+                backgroundSize: '600px'
+            }} class="footer p-10">
+                <div>
+                    <span class="footer-title">Services</span>
+                    <a class="link link-hover">Branding</a>
+                    <a class="link link-hover">Design</a>
+                    <a class="link link-hover">Marketing</a>
+                    <a class="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span class="footer-title">Company</span>
+                    <a class="link link-hover">About us</a>
+                    <a class="link link-hover">Contact</a>
+                    <a class="link link-hover">Jobs</a>
+                    <a class="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span class="footer-title">Legal</span>
+                    <a class="link link-hover">Terms of use</a>
+                    <a class="link link-hover">Privacy policy</a>
+                    <a class="link link-hover">Cookie policy</a>
+                </div>
+            </footer>
+            <div>
+
+                <div className='text-center my-5'>
+                    <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import DentalCare from './DentalCare';
+import Footer from './Footer';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import Services from './Services';
@@ -15,6 +16,7 @@ const Home = () => {
             <DentalCare></DentalCare>
             <MakeAppointment></MakeAppointment>
             <Testimonial></Testimonial>
+            <Footer></Footer>
         </div>
     );
 };
