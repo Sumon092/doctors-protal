4f095cf1431db49bbf42a786e1ab92836508b985
sumon092 <m.sumon@outlook.com>
2022-05-13 02:42:53 +0600

    handle submit added on modal

--- src/pages/Appointment/BookingModal.js
+++ src/pages/Appointment/BookingModal.js
@@ -1,25 +1,31 @@
 import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({ treatment, date }) => {
+const BookingModal = ({ treatment, date, setTreatment }) => {
     const { name, slots } = treatment;
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const slot = event.target.slot.value;
+        console.log(slot, name);
+        setTreatment(null)
+    }
     return (
         <div>
             <input type="checkbox" id="my-modal-6" class="modal-toggle" />
             <div class="modal modal-bottom sm:modal-middle">
                 <div class="modal-box">
                     <label for="my-modal-6" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 class="font-bold text-lg text-secondary">Do You Want to Book {name}?</h3>
-                    <form className='grid grid-cols-1 justify-items-center gap-5 mt-5'>
+                    <h3 class="font-bold text-lg text-secondary text-center">Do You Want to Book {name}?</h3>
+                    <form onSubmit={handleSubmit} className='grid grid-cols-1 justify-items-center gap-5 mt-5'>
                         <input type="text" value={format(date, 'PP')} disabled class="input input-bordered w-full max-w-xs" />
-                        <select class="select select-bordered w-full max-w-xs">
+                        <select name='slot' class="select select-bordered w-full max-w-xs">
                             {
                                 slots.map(slot => <option value={slot}>{slot}</option>)
                             }
                         </select>
-                        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                        <input type="text" name='name' placeholder="Enter Name" class="input input-bordered w-full max-w-xs" />
+                        <input type="email" name='email' placeholder="Enter Email" class="input input-bordered w-full max-w-xs" />
+                        <input type="text" name='phone' placeholder="Enter Phone No." class="input input-bordered w-full max-w-xs" />
                         <input type="submit" value='submit' className='btn btn-secondary w-full max-w-xs' />
                     </form>
                 </div>
--- src/pages/Appointment/Service.js
+++ src/pages/Appointment/Service.js
@@ -5,13 +5,13 @@ const Service = ({ service, setTreatment }) => {
     const { name, slots } = service;
     return (
         <div class="card lg:max-w-lg shadow-xl justify-center">
-            <div class="card-body">
+            <div class="card-body text-center">
                 <h2 class="card-title justify-center text-primary font-bold">{name}</h2>
                 <p>{
                     slots.length > 0
                         ? <span>{slots[0]}</span> : <span className='text-red-400'>Try Another Date</span>
                 }</p>
-                <p>
+                <p >
                     {slots.length}{slots.length > 1 ? 'spaces' : 'space'} available
                 </p>
                 <div class="card-actions justify-center">
