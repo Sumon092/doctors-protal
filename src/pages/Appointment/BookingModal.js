fe66295af0dc51a72d738ecfc62bee3cc977eca1
sumon092 <m.sumon@outlook.com>
2022-05-13 02:10:51 +0600

    booking modal added

--- src/pages/Appointment/BookingModal.js
+++ src/pages/Appointment/BookingModal.js
@@ -1,6 +1,7 @@
+import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({ treatment }) => {
+const BookingModal = ({ treatment, date }) => {
     const { name, slots } = treatment;
     return (
         <div>
@@ -9,10 +10,18 @@ const BookingModal = ({ treatment }) => {
                 <div class="modal-box">
                     <label for="my-modal-6" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 class="font-bold text-lg text-secondary">Do You Want to Book {name}?</h3>
-                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                    <div class="modal-action">
-                        <label for="my-modal-6" class="btn">Yay!</label>
-                    </div>
+                    <form className='grid grid-cols-1 justify-items-center gap-5 mt-5'>
+                        <input type="text" value={format(date, 'PP')} disabled class="input input-bordered w-full max-w-xs" />
+                        <select class="select select-bordered w-full max-w-xs">
+                            <option disabled selected>Who shot first?</option>
+                            <option>Han Solo</option>
+                            <option>Greedo</option>
+                        </select>
+                        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                        <input type="submit" value='submit' className='btn btn-secondary w-full max-w-xs' />
+                    </form>
                 </div>
             </div>
         </div>
